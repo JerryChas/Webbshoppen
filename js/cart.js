e6264d365d79b6288a1fc23ed64c0090d3117553
Viktor Magnusson <viktor.kuddis@hotmail.com>
2023-12-08 12:56:25 +0100

    pay confirmation and emty cart

--- js/cart.js
+++ js/cart.js
@@ -9,34 +9,53 @@ const emptyCart = document.getElementById('empty-cart');
 
 renderCartItems();
 
-//Knapp som tömmer localstorage
-// emptyCart.addEventListener('click', () => {
-//   localStorage.removeItem('cart');
-//   console.log('tömmer varukorg');
-//   console.log(`cart: ${cart}`)
-//   renderCartItems();
-  
-// });
+// Knapp som tömmer localstorage
+emptyCart.addEventListener('click', () => {
+    localStorage.removeItem('cart');
+    console.log('tömmer varukorg');
+    console.log(`cart: ${cart}`)
+    productContainer.innerHTML = "";
+    totalPrice.textContent = "0 kr";
+
+});
 
 
 
 
 function renderCartItems() {
-const productCards = cart.map((p) => {
-    return `
+    const productCards = cart.map((p) => {
+        return `
                   <div class="product-card">
                   <h3>${p.name}</h3>
                   <p>Price: ${p.price} kr</p>
                   </div>
                   `;
-  });
-  productContainer.innerHTML = productCards.join('');
+    });
+    productContainer.innerHTML = productCards.join('');
 
 }
 
 
 sumPrice = cart.reduce((accu, item) => {
     return accu + item.price
-}, 0) 
+}, 0)
 console.log(sumPrice)
-totalPrice.textContent = sumPrice;
+totalPrice.textContent = `${sumPrice}`;
+
+
+//Betala
+const payBtn = document.getElementById("pay");
+console.log(payBtn);
+payBtn.addEventListener("click", () => {
+    alert(`
+Okej Tack för din beställning!! \n 🚚 dina varor är påväg`);
+    localStorage.removeItem("cart");
+    productContainer.innerHTML = "";
+    totalPrice.textContent = "0 kr";
+
+
+    console.log(cart);
+
+
+
+})
