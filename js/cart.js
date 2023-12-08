a5a34ac91a7cd90a18e84f23e755e3bf355d3f01
jerryChas <jerry.olsson@chasacademy.se>
2023-12-08 12:21:07 +0100

    rendering cart on page

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,35 @@
 //Hämta produkter från local storage
+const productContainer = document.getElementById('product-container');
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-let varukorg = JSON.parse(localStorage.getItem("cart"));
-
-console.table(varukorg)
+console.table(cart);
 
 const emptyCart = document.getElementById('empty-cart');
 
+renderCartItems();
+
 //Knapp som tömmer localstorage
-emptyCart.addEventListener('click', () => {
-    localStorage.removeItem('cart');
-    console.log('tömmer varukorg')
-});
+// emptyCart.addEventListener('click', () => {
+//   localStorage.removeItem('cart');
+//   console.log('tömmer varukorg');
+//   console.log(`cart: ${cart}`)
+//   renderCartItems();
+  
+// });
+
+
+
+
+function renderCartItems() {
+const productCards = cart.map((p) => {
+    return `
+                  <div class="product-card">
+                  <h3>${p.name}</h3>
+                  <p>Price: ${p.price} kr</p>
+                  </div>
+                  `;
+  });
+  productContainer.innerHTML = productCards.join('');
+
+}
 
